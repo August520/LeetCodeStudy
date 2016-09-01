032f012835b030c177702883af651afa002120d5
August <majun_6513@126.com>
2016-09-01 17:14:34 +0800

    modify: sort but wrong

--- TwoSum_2/TwoSum_2/Solution.cpp
+++ TwoSum_2/TwoSum_2/Solution.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,7 +8,31 @@ class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) 
 	{
+		sort(nums.begin(), nums.end());
+		unsigned int first = 0;
+		unsigned int last = nums.size() - 1;
+		int sum = 0;
+
 		vector<int> vecTwoSum;
+
+		while (target != sum)
+		{
+			sum = nums[first] + nums[last];
+			if (sum > target)
+				last--;
+			else if (sum < target)
+				first++;
+			else
+			{
+				vecTwoSum.push_back(first);
+				vecTwoSum.push_back(last);
+				return vecTwoSum;
+			}
+				
+		}
+		return vecTwoSum;
+
+		
 		for (unsigned int i=0; i<nums.size(); i++)
 			for (unsigned int j=i+1; j<nums.size(); j++)
 			{
